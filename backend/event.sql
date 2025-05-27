715c1e1d5b09fb1249993ff98192a8a925adfd51
Giabao1292 <giabao362004@gmail.com>
2025-05-27 21:03:49 +0700

    Update event.sql

--- backend/event.sql
+++ backend/event.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `tbl_address`
 --
-
+CREATE SCHEMA event;
+Use event;
 DROP TABLE IF EXISTS `tbl_address`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
