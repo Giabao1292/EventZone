049534e97f2b652bbc75773a63b02d66c345cd14
Giabao1292 <giabao362004@gmail.com>
2025-06-02 12:03:25 +0700

    Update column email from tbl_verification_token

--- updatedb.sql
+++ updatedb.sql
@@ -1,12 +1,3 @@
-ALTER TABLE user DROP COLUMN enabled;
-
-CREATE TABLE tbl_verification_token (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    token VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    expiry_date DATETIME(6) NOT NULL,
-    INDEX expiry_date_idx (expiry_date)
-);
 
 ALTER TABLE tbl_user ADD COLUMN provider_id VARCHAR(255);
 
@@ -16,3 +7,13 @@ CREATE TABLE tbl_token (
     access_token TEXT,
     refresh_token TEXT
 );
+Drop TABLE tbl_verification_token;-- (Update email)
+CREATE TABLE tbl_verification_token (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    token VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    expiry_date DATETIME(6) NOT NULL,
+    INDEX expiry_date_idx (expiry_date)
+);
+
+ALTER TABLE user DROP COLUMN enabled;
