a9cc5ff862e7fdfb7ba099807307846f91701a58
TwKun11 <trikun2k4@gmail.com>
2025-06-10 22:25:48 +0700

    authorized front end

--- fe-event/src/ui/AppLayout.jsx
+++ fe-event/src/ui/AppLayout.jsx
@@ -1,15 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import PropTypes from "prop-types";
+import { Outlet } from "react-router-dom"; // <-- Thêm Outlet
 
 const AppLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-[#0A0A0A]">
       <Header />
       <main className="flex-grow from-slate-100 to-slate-200">
-        {" "}
-        {/* Giữ nguyên hoặc thay đổi tùy ý */}
-        {children}
+        {children ? children : <Outlet />}
       </main>
       <Footer />
     </div>
@@ -17,7 +16,7 @@ const AppLayout = ({ children }) => {
 };
 
 AppLayout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
 };
 
 export default AppLayout;
