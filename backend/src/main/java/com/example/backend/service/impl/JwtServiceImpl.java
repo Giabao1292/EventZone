bf3988d95964d1da9650651e3954c381809826f9
Giabao1292 <giabao362004@gmail.com>
2025-07-06 12:29:05 +0700

    Error jwt must be not display on console

--- backend/src/main/java/com/example/backend/service/impl/JwtServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/JwtServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.backend.service.impl;
 import com.example.backend.service.JwtService;
 import com.example.backend.util.TokenType;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -73,12 +74,16 @@ public class JwtServiceImpl implements JwtService {
     }
 
     private Claims getClaimsFromToken(String token, TokenType type) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getKey(type))
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
 
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(getKey(type))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (JwtException e) {
+            return null;
+        }
     }
 
     private <T> T extractClaimsFromToken(String token, Function<Claims, T> claimsResolver, TokenType type) {
